e642f10907a724b22eff7b2f03524db1290942be
Ohad Frenkel Keshet <ohad.frenkel@feedvisor.com>
2022-08-08 17:08:35 +0300

    Moving transport and client outside of disposable scopes
    * According to the client's documentation, it is advisable to only have one `HttpApiTransport` and `ArangoDBClient` and not use `use` which disposes of them once out of scope.
    * Moved the connection details into a mutable, global, variables inside their own module.

---
+++ connectionDetails.fs
@@ -0,0 +1,6 @@
+module ConnectionDetails
+
+let mutable url = "http://localhost:8529"
+let mutable username = "root"
+let mutable password = "openSesame"
+let mutable dbName = "_system"
--- databases.fs
+++ databases.fs
@@ -4,92 +4,80 @@ open ArangoDBNetStandard
 open ArangoDBNetStandard.Transport.Http
 open ArangoDBNetStandard.DatabaseApi.Models
 open System
+open ConnectionDetails
 
-let mutable url = "http://localhost:8529"
-let mutable username = "root"
-let mutable password = "openSesame"
-let mutable dbName = "_system"
+let dbTransport =
+  HttpApiTransport.UsingBasicAuth(new Uri(url), dbName, username, password)
+
+let db = new ArangoDBClient(dbTransport)
+
 
 let getCurrentDBInfo () =
-    try
-        use transport =
-            HttpApiTransport.UsingBasicAuth(new Uri(url), dbName, username, password)
+  try
+    let response =
+      db
+        .Database
+        .GetCurrentDatabaseInfoAsync()
+        .GetAwaiter()
+        .GetResult()
 
-        use db = new ArangoDBClient(transport)
-
-        let response =
-            db
-                .Database
-                .GetCurrentDatabaseInfoAsync()
-                .GetAwaiter()
-                .GetResult()
-
-        Ok response
-    with
-    | :? Net.Http.HttpRequestException
-    | :? ApiErrorException
-    | :? UriFormatException as ex -> Error ex
+    Ok response
+  with
+  | :? Net.Http.HttpRequestException
+  | :? ApiErrorException
+  | :? UriFormatException as ex -> Error ex
 
 let getAllDBs () =
-    try
-        use transport =
-            HttpApiTransport.UsingBasicAuth(new Uri(url), dbName, username, password)
+  try
+    use db = new ArangoDBClient(dbTransport)
 
-        use db = new ArangoDBClient(transport)
+    let response =
+      db
+        .Database
+        .GetDatabasesAsync()
+        .GetAwaiter()
+        .GetResult()
 
-        let response =
-            db
-                .Database
-                .GetDatabasesAsync()
-                .GetAwaiter()
-                .GetResult()
-
-        Ok response
-    with
-    | :? Net.Http.HttpRequestException
-    | :? ApiErrorException
-    | :? UriFormatException as ex -> Error ex
+    Ok response
+  with
+  | :? Net.Http.HttpRequestException
+  | :? ApiErrorException
+  | :? UriFormatException as ex -> Error ex
 
 let setupDBUserCreds username password active =
-    new DatabaseUser(Username = username, Passwd = password, Active = active)
+  new DatabaseUser(Username = username, Passwd = password, Active = active)
 
 let setupNewDB dbName users =
-    new PostDatabaseBody(Name = dbName, Users = users)
+  new PostDatabaseBody(Name = dbName, Users = users)
 
 let createDB (newDBName: string) (newDBUsers: seq<DatabaseUser>) =
-    try
-        use transport =
-            HttpApiTransport.UsingBasicAuth(new Uri(url), dbName, username, password)
+  try
+    use db = new ArangoDBClient(dbTransport)
+    let body = new PostDatabaseBody(Name = newDBName, Users = newDBUsers)
 
-        use db = new ArangoDBClient(transport)
-        let body = new PostDatabaseBody(Name = newDBName, Users = newDBUsers)
-
-        Ok
-        <| db
-            .Database
-            .PostDatabaseAsync(body)
-            .GetAwaiter()
-            .GetResult()
-    with
-    | :? Net.Http.HttpRequestException
-    | :? ApiErrorException
-    | :? UriFormatException as ex -> Error ex
+    Ok
+    <| db
+      .Database
+      .PostDatabaseAsync(body)
+      .GetAwaiter()
+      .GetResult()
+  with
+  | :? Net.Http.HttpRequestException
+  | :? ApiErrorException
+  | :? UriFormatException as ex -> Error ex
 
 
 let deleteDB (dbToDelete: string) =
-    try
-        use transport =
-            HttpApiTransport.UsingBasicAuth(new Uri(url), dbName, username, password)
+  try
+    use db = new ArangoDBClient(dbTransport)
 
-        use db = new ArangoDBClient(transport)
-
-        Ok
-        <| db
-            .Database
-            .DeleteDatabaseAsync(dbToDelete)
-            .GetAwaiter()
-            .GetResult()
-    with
-    | :? Net.Http.HttpRequestException
-    | :? ApiErrorException
-    | :? UriFormatException as ex -> Error ex
+    Ok
+    <| db
+      .Database
+      .DeleteDatabaseAsync(dbToDelete)
+      .GetAwaiter()
+      .GetResult()
+  with
+  | :? Net.Http.HttpRequestException
+  | :? ApiErrorException
+  | :? UriFormatException as ex -> Error ex
